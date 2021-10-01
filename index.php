88f661717f10c7ca748e3cf7ad2ba7b30aba8b1a
ArtGouaux <90456855+ArtGouaux@users.noreply.github.com>
2021-10-01 16:27:31 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Arthur" => [
+        "city" => "Orléans",
+        "weapon" => "hache à deux mains"
+    ]
+];
+
+var_dump($characters);
